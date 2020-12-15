e15d090f2ca8a811b1d00599d40277d164cd0e46
Jacob Rozran <rozran00@gmail.com>
2020-12-15 13:06:02 -0500

    learning some blockchain

---
+++ test.R
@@ -0,0 +1,14 @@
+library(jsonlite)
+library(httr)
+
+GET("http://localhost:5000/mine")
+
+string <- toJSON(list(sender = "d4ee26eee15148ee92c6cd394edd974e",
+                      recipient = "someone-other-address",
+                      amount = 5), auto_unbox = TRUE)
+
+test <- POST(url = "http://localhost:5000/transactions/new", 
+             add_headers(`content_type` = sprintf("application/json")),
+             body = string)
+
+prettify(GET("http://localhost:5000/chain"))
