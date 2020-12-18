1f53c3a6829074ff04099887f1c3d6ade74509d1
--global <rozran00@gmail.com>
2020-12-17 21:13:32 -0500

    messing with the hash algorithm

--- hash_practice.py
+++ hash_practice.py
@@ -1,6 +1,17 @@
 from hashlib import sha256
+import datetime
+from decimal import Decimal
+
+start = Decimal(datetime.datetime.now().strftime('%s.%f'))
 x = 5
 y = 0  # We don't know what y should be yet...
-while sha256(f'{x*y}'.encode()).hexdigest()[-1] != "0":
+
+while sha256(f'{x*y}'.encode()).hexdigest()[-7:] != "0000000":
     y += 1
+
+end = Decimal(datetime.datetime.now().strftime('%s.%f'))
+time = end - start
+
 print(f'The solution is y = {y}')
+print(f'It took {time} seconds to generate this answer.')
+print(sha256(f'{x*y}'.encode()).hexdigest())
